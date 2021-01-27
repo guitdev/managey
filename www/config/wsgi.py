be033c1f58427f1f8ff19df5d611c4657e4ad656
Guit <ggojard@gmail.com>
2021-01-27 16:04:28 +0100

    First commit

---
+++ www/config/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for config project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+this_file = "venv/bin/activate_this.py"
+exec(open(this_file).read(), {'__file__': this_file})
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = get_wsgi_application()
---
+++ www/polls/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    # ex: /polls/
+    # ex: /polls/5/
+    # ex: /polls/5/results/
+    # ex: /polls/5/vote/
