f2c158ce7112370fb4d0f74fd1552e5810370b93
Guit <ggojard@gmail.com>
2021-02-18 09:43:26 +0100

    move django to secrets

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=-d^^_&in!xlvk2^b0t9g-@dnhs9%b8ushs&q98o176ut$*e1b'
+SECRET_KEY = ${{ secrets.OVH_HOSTING_USER }}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #False
